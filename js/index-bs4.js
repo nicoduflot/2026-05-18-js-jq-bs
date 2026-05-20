b3a02e92dab60632abddb0b71db76512e2793656
unknown <nduflot@dawan.fr>
2026-05-20 13:50:44 +0200

    bootstrap et jquery les alerts bouton affichage

--- js/index-bs4.js
+++ js/index-bs4.js
@@ -1,6 +1,6 @@
 $(function(){
     /* répérer le clic sur le bouton welcomeButton */
 
-    /* l'élément last-info s'affiche ou pas à l'aide de la classe show */
-    
+    /* l'élément last-info s'affiche ou pas à l'aide de la classe show et d-none manipulée via le clic sur le bouton info */
+
 });
