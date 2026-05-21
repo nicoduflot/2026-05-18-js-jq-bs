ab07f8a0acda139c1fa8d0bcdd9e616d3f3af2f4
unknown <nduflot@dawan.fr>
2026-05-21 09:04:58 +0200

    revision html css rwd

--- js/index-bs4.js
+++ js/index-bs4.js
@@ -28,6 +28,7 @@ $(function () {
 
     function parseXmlBooks(data, target) {
         /*console.log($(xml));*/
+        $(target).empty();
         $(data).find('book').each(function () {
             const category = $(this).attr('category');
             const title = $(this).find('title').text();
@@ -36,11 +37,39 @@ $(function () {
             /* Complétez la suite de la récupération des information et créez un affichage
             mettez chaque article dans une div avec les classes col-12 col-md-4 col-lg-3
             <div class="col-12 col-md-4 col-lg-3">
-                <article>
-                </article>
+            <article>
+            </article>
             </div>
             */
+           const divAuthor = $('<div>', {
+            class: 'card-footer'
+           });
+           authors.each(function(){
+               const author = $(this).text();
+               const spanAuthor = $('<span>').text(author);
+               divAuthor.append(spanAuthor, $('<br />'));    
+            });
+            const year = $(this).find('year').text();
+            const price = $(this).find('price').text();
+            const divBook = $('<div>', {
+                class: 'col-12 col-md-4 col-lg-3 my-2'
+            });
+            const article = $('<article>', {
+                class: 'card'
+            });
+            
+            const cardBody = $('<div>', {
+                class: 'card-body'
+            });
+            const cardTitle = $('<h5>', {
+                class: 'card-title'
+            }).text(title);
+            const pYear = $('<p>').html(`<b>Année de publication :</b> ${year}`);
+            const pPrice = $('<p>').html(`<b>Prix public :</b> ${price} £`);
+            divBook.append(article);
+            article.append(cardBody.append(cardTitle, pYear, pPrice), divAuthor);
 
+            $(target).append(divBook);
         });
     }
 
